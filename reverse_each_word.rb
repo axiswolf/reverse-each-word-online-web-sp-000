4ec3aa20ccd2c10182914219fe8bf851eab4b174
Karen V <karenvo@usc.edu>
2019-02-08 16:14:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,6 @@
 def reverse_each_word(sentence)
-  sentence.split(" ")
-  word = []
-  sentence.each do |sent|
-    word << sentence.reverse
-    word.join(" ")
-  end
-  word
+  words = sentence.split(" ")
+  array = []
+  array = words.collect {|word| word.reverse}
+  array.join(" ")
 end
